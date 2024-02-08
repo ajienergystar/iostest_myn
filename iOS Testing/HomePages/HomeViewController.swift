c1a2be0a407bd9aa1c8b76732e81a6ee7d6254db
Aji Prakosa <ajienergystar@gmail.com>
2024-02-08 10:24:57 +0700

    Show List Data Avengers in HomePage

--- iOS Testing/HomePages/HomeViewController.swift
+++ iOS Testing/HomePages/HomeViewController.swift
@@ -7,23 +7,54 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableData: UITableView!
+    
+    var menuItems: NSArray = [
+        ["Image" : "superman", "Title" : "Superman", "Rating": "Very Good"],
+        ["Image" : "avenger_ironman", "Title" : "Iron Man", "Rating": "Normal"],
+        ["Image" : "avenger_hulk", "Title" : "Hulk", "Rating": "Awesome"],
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableData.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+        self.tableData.delegate = self
+        self.tableData.dataSource = self
+        self.tableData.separatorStyle = .none
+        self.tableData.tableFooterView = UIView(frame: CGRect.zero)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 117
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath as IndexPath) as! TableViewCell
+        
+        
+        let images: String = String(describing: (menuItems.value(forKey: "Image") as! NSArray).object(at: indexPath.row))
+        let titles: String = String(describing: (menuItems.value(forKey: "Title") as! NSArray).object(at: indexPath.row))
+        let ratings: String = String(describing: (menuItems.value(forKey: "Rating") as! NSArray).object(at: indexPath.row))
+
+
+        cell.selectionStyle = .none
+        cell.configureView(imageData: images, name: titles, rating: ratings)
+        cell.accessoryType = .disclosureIndicator
+
+        return cell
+        
+    }
 
 }
